9f0e1e5a231b96167c19d5ca989a5b68fe21777c
cornelia-rus <cornelia.ardelean@sfappworks.com>
2024-08-22 19:11:31 +0300

    Create app.js
    on mouse move animation

---
+++ app.js
@@ -0,0 +1,13 @@
+let circle = document.getElementById('testimonials-hero-glow');
+
+const onMouseMove = (e) => {
+  // Calculate the center coordinates of the circle
+  let centerX = e.pageX - (circle.offsetWidth / 2);
+  let centerY = e.pageY - (circle.offsetHeight / 2);
+
+  // Update the position of the circle based on the center coordinates
+  circle.style.left = centerX + 'px';
+  circle.style.top = centerY + 'px';
+}
+
+document.addEventListener('mousemove', onMouseMove);
